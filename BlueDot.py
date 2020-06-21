ddbb36e2cd6c89a7ec806ccbaefcf710bd03d253
kdavina <37858141+kdavina@users.noreply.github.com>
2020-06-20 23:11:06 -0600

    BlueDot file

---
+++ BlueDot.py
@@ -0,0 +1,43 @@
+from bluedot import BlueDot
+from picamera import PiCamera
+import time
+import os
+
+def take_picture():
+    global last_video_pic
+    last_video_pic = '/home/pi/Pictures/image_' + time.strftime('%Y-%m-%d....') + '.jpg'
+    cam.capture(".jpg")
+
+def stop_program():
+    cam.stop_preview()
+    global continue_program
+    continue_program = False
+    bd.stop()
+    print("continue_program", continue_program)
+
+def record_video():
+    global started_video
+    global last_video_pic
+    if started_video:
+        cam.stop_recording()
+        started_video = False
+        os.remove(last_video_pic)
+        print("stopped video. started_video=", started_video)
+    else:
+        cam.start_recording('name_of_your_video.h264')
+        last_video_pic = '/home/pi/Pictures/image_' + time.strftime('%Y-%m-%d....') + '.jpg'
+        print("started video. started_video=", started_video)
+
+bd = BlueDot()
+cam = PiCamera()
+cam.start_preview()
+started_video = False
+last_video_pic = ''
+bd.when_double_pressed = record_video
+bd.set_when_pressed(take_picture, background=True)
+bd.when_swiped = stop_program
+
+
+
+
+
